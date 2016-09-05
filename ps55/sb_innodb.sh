6bbb8e63c1ba20e279f84b275e7accfcf267a100
Peter Zaitsev <pz@percona.com>
2016-09-06 00:14:21 +0200

    added ps55

---
+++ ps55/sb_innodb.sh
@@ -0,0 +1,5 @@
+while true
+do
+    sysbench --tx-rate=10  --num-threads=64 --report-interval=10 --max-time=0 --max-requests=0 --rand-type=pareto --oltp-table-size=100000000  --mysql-user=sbtest --mysql-password=sbtest  --mysql-db=innodb --oltp-reconnect --test=/usr/share/doc/sysbench/tests/db/oltp.lua run > /dev/null
+    sleep 60
+done
