506be0fe6045b8b583bf3a898646e5482961650d
Peter Zaitsev <pz@percona.com>
2016-09-12 01:21:02 +0200

    Add periodic large update transaction (non conflicting)

--- grants.sql
+++ grants.sql
@@ -1,3 +1,3 @@
 grant all privileges on *.* to sbtest@localhost identified by 'sbtest';
-GRANT ALL ON innodb.* to 'sbtest'@'10.%' identified by 'sbtest';
+GRANT ALL ON *.* to 'sbtest'@'10.%' identified by 'sbtest';
 
--- pxc56-1/start_all.sh
+++ pxc56-1/start_all.sh
@@ -3,5 +3,7 @@ sleep 60
 nohup sh ./sb_innodb_spike.sh > /dev/null &
 sleep 200
 nohup sh ./extra_innodb.sh > /dev/null &
+sleep 450
+nohup sh ./large_update.sh > /dev/null &
 sleep 5
 echo "All Started!"
