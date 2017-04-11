9224732b19070ee48ee00ead06d3283f7f12c927
Mykola Marzhan <delgod@delgod.com>
2017-04-11 15:16:18 +0300

    PMM-715 minor style improvement

--- mdb101/extra_innodb.sh
+++ mdb101/extra_innodb.sh
@@ -1,6 +1,14 @@
-while true
-do
+#!/bin/sh
+
+while true; do
     RND=$(( RANDOM % 7000 * 10000 ))
-    mysql -u sbtest -psbtest -e "select avg(length(c)) from innodb.sbtest1 where id between $RND and $RND+30000000" > /dev/null
+    mysql \
+        -u sbtest \
+        -psbtest \
+        -e "
+            SELECT avg(length(c))
+            FROM   innodb.sbtest1
+            WHERE  id between ${RND} and ${RND}+30000000
+        " > /dev/null
     sleep 1000
 done
--- mdb101/sb_innodb_spike.sh
+++ mdb101/sb_innodb_spike.sh
@@ -1,5 +1,18 @@
-while true
-do
-	sysbench   --num-threads=64 --report-interval=10 --max-time=300 --max-requests=0 --rand-type=pareto --oltp-table-size=100000000  --mysql-user=sbtest --mysql-password=sbtest  --mysql-db=innodb --oltp-reconnect --test=/usr/share/doc/sysbench/tests/db/update_index.lua  run   >> /dev/null
+#!/bin/sh
+
+while true; do
+    sysbench \
+        --num-threads=64 \
+        --report-interval=10 \
+        --max-time=300 \
+        --max-requests=0 \
+        --rand-type=pareto \
+        --oltp-table-size=100000000 \
+        --mysql-user=sbtest \
+        --mysql-password=sbtest \
+        --mysql-db=innodb \
+        --oltp-reconnect \
+        --test=/usr/share/doc/sysbench/tests/db/update_index.lua \
+        run > /dev/null
 	sleep 3300
 done
