6e5472be4164e17c70b3b7c8dd5851db4f2cb2d6
Mykola Marzhan <delgod@delgod.com>
2017-04-11 15:34:14 +0300

    PMM-715 use sysbench-1.0.x syntax

--- mdb101/sb_innodb.sh
+++ mdb101/sb_innodb.sh
@@ -7,18 +7,18 @@ SOCKET=${1:-/var/lib/mysql/mysql.sock}
 while true; do
     sysbench \
         --mysql-socket=$SOCKET \
-        --tx-rate=20 \
-        --num-threads=64 \
+        --rate=20 \
+        --threads=64 \
         --report-interval=10 \
-        --max-time=0 \
-        --max-requests=0 \
+        --time=0 \
+        --events=0 \
         --rand-type=pareto \
         --oltp-table-size=100000000 \
         --mysql-user=sbtest \
         --mysql-password=sbtest \
         --mysql-db=innodb \
         --oltp-reconnect \
-        --test=/usr/share/doc/sysbench/tests/db/oltp.lua \
+        /usr/share/sysbench/tests/include/oltp_legacy/oltp.lua \
         run
     sleep 60
 done
--- mdb101/sb_innodb_spike.sh
+++ mdb101/sb_innodb_spike.sh
@@ -7,17 +7,17 @@ SOCKET=${1:-/var/lib/mysql/mysql.sock}
 while true; do
     sysbench \
         --mysql-socket=$SOCKET \
-        --num-threads=64 \
+        --threads=64 \
         --report-interval=10 \
-        --max-time=300 \
-        --max-requests=0 \
+        --time=300 \
+        --events=0 \
         --rand-type=pareto \
         --oltp-table-size=100000000 \
         --mysql-user=sbtest \
         --mysql-password=sbtest \
         --mysql-db=innodb \
         --oltp-reconnect \
-        --test=/usr/share/doc/sysbench/tests/db/update_index.lua \
+        /usr/share/sysbench/tests/include/oltp_legacy/update_index.lua \
         run
 	sleep 3300
 done
