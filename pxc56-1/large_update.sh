c7a781088f77bdb746d6c420a1216060e9dd5f10
Mykola Marzhan <delgod@delgod.com>
2017-04-19 20:25:27 +0300

    add pxc nodes

--- pxc56-1/large_update.sh
+++
@@ -1,7 +0,0 @@
-# We run this one on pxc56-1 only and directly to simulate how large transactions impact cluster
-while true
-do
-    RND=$(( RANDOM % 1000 ))
-    mysql -u sbtest -psbtest -e "update innodb_small.sbtest1 set k=$RND" > /dev/null
-    sleep 950
-done
