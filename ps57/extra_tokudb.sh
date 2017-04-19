331158edec97495ed67cdd17177b41332dfc90d4
Mykola Marzhan <delgod@delgod.com>
2017-04-19 15:43:55 +0300

    setup ps55, ps56, ps57 via install script

--- ps57/extra_tokudb.sh
+++
@@ -1,6 +0,0 @@
-while true
-do
-    RND=$(( RANDOM % 8000 * 10000 ))
-    mysql -u sbtest -psbtest -e "select avg(length(c)) from myisam.sbtest1 where id between $RND and $RND+20000000" > /dev/null
-    sleep 1500
-done
