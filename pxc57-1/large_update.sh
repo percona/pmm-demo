e919ba6e427dd15f0e3b7438d127f93467264a44
Michael Coburn <michael.coburn@percona.com>
2017-04-19 23:15:27 -0500

    fix typo to correct hostname

--- pxc57-1/large_update.sh
+++ pxc57-1/large_update.sh
@@ -1,4 +1,4 @@
-# We run this one on pxc56-1 only and directly to simulate how large transactions impact cluster
+# We run this one on pxc57-1 only and directly to simulate how large transactions impact cluster
 while true
 do
     RND=$(( RANDOM % 1000 ))
