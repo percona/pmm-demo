0e18f5127673a733e68fa78a429438c6e97663d3
Michael Coburn <michael.coburn@percona.com>
2017-05-04 17:02:40 -0500

    bad pathing x2, making it requirement to be /home/ansible-deploy/pmm-demo

--- db_generate/create_innodb_icp.sh
+++ db_generate/create_innodb_icp.sh
@@ -1,2 +1,2 @@
-sysbench  --tx-rate=50 --num-threads=200 --report-interval=10 --max-time=0 --max-requests=0 --rand-type=pareto --oltp-table-size=100000  --mysql-user=sbtest --mysql-password=sbtest  --mysql-db=innodb_icp --test=~/pmm-demo/tests-0.5/select_random_ranges.lua prepare
+sysbench  --tx-rate=50 --num-threads=200 --report-interval=10 --max-time=0 --max-requests=0 --rand-type=pareto --oltp-table-size=100000  --mysql-user=sbtest --mysql-password=sbtest  --mysql-db=innodb_icp --test=/home/ansible-deploy/pmm-demo/tests-0.5/select_random_ranges.lua prepare
 
