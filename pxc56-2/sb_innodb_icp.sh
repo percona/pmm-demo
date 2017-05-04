28747a847a520711a748f64d71708ec4b2e353ce
Michael Coburn <michael.coburn@percona.com>
2017-05-04 17:28:33 -0500

    bad pathing now for actual test, making it requirement to be /home/ansible-deploy/pmm-demo

--- pxc56-2/sb_innodb_icp.sh
+++ pxc56-2/sb_innodb_icp.sh
@@ -1,5 +1,5 @@
 while true
 do
-    sysbench  --tx-rate=50 --num-threads=200 --report-interval=10 --max-time=0 --max-requests=0 --rand-type=pareto --oltp-table-size=100000  --mysql-user=sbtest --mysql-password=sbtest  --mysql-db=innodb_icp --test=../tests-0.5/select_random_ranges.lua run > /dev/null
+    sysbench  --tx-rate=50 --num-threads=200 --report-interval=10 --max-time=0 --max-requests=0 --rand-type=pareto --oltp-table-size=100000  --mysql-user=sbtest --mysql-password=sbtest  --mysql-db=innodb_icp --test=/home/ansible-deploy/tests-0.5/select_random_ranges.lua run > /dev/null
     sleep 120
 done
