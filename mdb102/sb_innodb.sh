5f1899225ff3cde06509a7912c9cd37ab48aad7b
Michael Coburn <michael.coburn@percona.com>
2017-07-05 13:22:12 -0500

    cleaning up directories I missed in merge from upstream

--- mdb102/sb_innodb.sh
+++
@@ -1,5 +0,0 @@
-while true
-do
-    sysbench --tx-rate=20  --num-threads=64 --report-interval=10 --max-time=0 --max-requests=0 --rand-type=pareto --oltp-table-size=100000000  --mysql-user=sbtest --mysql-password=sbtest  --mysql-db=innodb --oltp-reconnect --test=/usr/share/doc/sysbench/tests/db/oltp.lua run > /dev/null
-    sleep 60
-done
